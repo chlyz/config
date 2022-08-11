d3a7018f42d45168d8afc106ee32953eae1a6642
Christian Lyzell <christian.lyzell@gmail.com>
2022-08-11 22:04:20 +0200

    nvim: more appealing 'listchars'

--- .config/nvim/lua/lyzell/options.lua
+++ .config/nvim/lua/lyzell/options.lua
@@ -28,13 +28,14 @@ vim.opt.fillchars = {
   vert            = '┃',                              -- BOX DRAWINGS HEAVY VERTICAL (U+2503, UTF-8: E2 94 83)
 }
 
-vim.opt.list           = true                              -- show whitespace
+vim.opt.list           = true
 vim.opt.listchars      = {
-  nbsp                 = '⦸',                              -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
-  extends              = '»',                              -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
-  precedes             = '«',                              -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
-  tab                  = '▷⋯',                             -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
-  trail                = '•',                              -- BULLET (U+2022, UTF-8: E2 80 A2)
+  eol                  = '↲',
+  nbsp                 = '␣',
+  extends              = '»',
+  precedes             = '«',
+  tab                  = '» ',
+  trail                = '•',
 }
 
 vim.opt.shell = 'sh'
