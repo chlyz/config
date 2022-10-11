96eb65d53e3297d10c9742f6a3f56ec8d265f95a
Christian Lyzell <christian.lyzell@gmail.com>
2022-10-11 23:34:57 +0200

    nvim: simplify the keymaps expressions.

--- .config/nvim/lua/lyzell/keymaps.lua
+++ .config/nvim/lua/lyzell/keymaps.lua
@@ -5,30 +5,15 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader><leader>', '<C-6>')
 
 -- Save and quit using simpler commands.
-vim.keymap.set('n', '<C-S>', function() vim.cmd("write") end)
+vim.keymap.set('n', '<C-S>', ':write<CR>')
 vim.keymap.set('i', '<C-S>', '<Esc>:write<CR>')
-vim.keymap.set('n', '<C-Q>', function() vim.cmd("quit") end)
-vim.keymap.set('n', '<C-G>x', function() vim.cmd("xit") end)
-vim.keymap.set('n', 'gq', function() vim.cmd("bdelete") end)
+vim.keymap.set('n', '<C-Q>', ':quit<CR>')
+vim.keymap.set('n', 'gq',    ':bdelete<CR>')
 
 -- Center the cursor after some mappings.
 vim.keymap.set('n', '<C-T>', '<C-T>zz')
 vim.keymap.set('n', '<C-O>', '<C-O>zz')
 vim.keymap.set('n', '<C-I>', '<C-I>zz')
-
--- Replace awkward move to beginning of line.
-vim.keymap.set('n', '<C-H>', 'H')
-vim.keymap.set('n', 'H', '^')
-vim.keymap.set('n', 'cH', 'c^')
-vim.keymap.set('n', 'dH', 'd^')
-vim.keymap.set('n', 'yH', 'y^')
-vim.keymap.set('v', 'H', '^')
-
--- Replace awkward move to end of line.
-vim.keymap.set('n', '<C-L>', 'L')
-vim.keymap.set('n', 'L', '$')
-vim.keymap.set('v', 'L', '$')
-vim.keymap.set('n', 'yL', 'y$')
 
 -- Copy and pasting from the system clipboard.
 vim.keymap.set('n', 'gp', '"+pv`]=`>')
@@ -39,16 +24,18 @@ vim.keymap.set('n', 'gy', '"+y')
 vim.keymap.set('v', 'gy', '"+y')
 
 -- Fugitive bindings.
-vim.keymap.set('n', 'gs', function () vim.cmd("G") end)
-vim.keymap.set('n', 'gl', function () vim.cmd("Git log") end)
-vim.keymap.set('n', 'gt', function ()
+vim.keymap.set('n', 'gs', ':G<CR>')
+vim.keymap.set('n', 'gl', ':Git log<CR>')
+vim.keymap.set('n', '<Leader>gT', function ()
+    vim.cmd('silent Ggrep! TODO')
+    vim.cmd('copen')
+end)
+vim.keymap.set('n', '<Leader>gt', function ()
     vim.cmd('silent Ggrep! TODO(chlyz)')
     vim.cmd('copen')
 end)
-vim.keymap.set('n', '<C-G>s', function () vim.cmd("G") end)
-vim.keymap.set('n', '<leader>gs', function () vim.cmd("G") end)
-vim.keymap.set('n', '<leader>gb', function () vim.cmd("Git blame") end)
-vim.keymap.set('n', '<leader>gm', function () vim.cmd("Gvdiffsplit dev/master") end)
+vim.keymap.set('n', '<leader>gb', ':Git blame<CR>')
+vim.keymap.set('n', '<leader>gm', ':Gvdiffsplit dev/master')
 vim.keymap.set('n', '<leader>gw', function ()
     local current_word = vim.call('expand', '<cword>')
     vim.cmd('silent Ggrep! ' .. current_word)
@@ -56,57 +43,14 @@ vim.keymap.set('n', '<leader>gw', function ()
 end)
 
 -- Fuzzy search files and buffers.
-vim.keymap.set('n', '<Leader>.', function()
-    require'telescope.builtin'.git_files(require('telescope.themes').get_dropdown({
-        layout_config = {
-            width = 0.8,
-            height = 0.5,
-        },
-    }))
-end)
-vim.keymap.set('n', '<Leader>>', function()
-    require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({
-        layout_config = {
-            width = 0.8,
-            height = 0.5,
-        },
-    }))
-end)
-vim.keymap.set('n', '<Leader>,', function()
-    require'telescope.builtin'.buffers(require('telescope.themes').get_dropdown({
-        layout_config = {
-            width = 0.8,
-            height = 0.5,
-        },
-    }))
-end)
-vim.keymap.set('n', '<Leader><', function()
-    vim.cmd("Telescope")
-end)
-vim.keymap.set('n', '<Leader>;', function()
-    require'telescope.builtin'.current_buffer_tags(require('telescope.themes').get_dropdown({
-        layout_config = {
-            width = 0.8,
-            height = 0.5,
-        },
-    }))
-end)
-vim.keymap.set('n', '<Leader>:', function()
-    require'telescope.builtin'.command_history(require('telescope.themes').get_dropdown({
-        layout_config = {
-            width = 0.8,
-            height = 0.5,
-        },
-    }))
-end)
-vim.keymap.set('n', '<Leader>/', function()
-    require'telescope.builtin'.live_grep(require('telescope.themes').get_dropdown({
-        layout_config = {
-            width = 0.8,
-            height = 0.5,
-        },
-    }))
-end)
+vim.keymap.set('n', '<C-h>',     ':Telescope help_tags<CR>')
+vim.keymap.set('n', '<Leader>.', ':Telescope git_files<CR>')
+vim.keymap.set('n', '<Leader>>', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<Leader>,', ':Telescope buffers<CR>')
+vim.keymap.set('n', '<Leader><', ':Telescope builtin<CR>')
+vim.keymap.set('n', '<Leader>;', ':Telescope current_buffer_tags<CR>')
+vim.keymap.set('n', '<Leader>:', ':Telescope command_history<CR>')
+vim.keymap.set('n', '<Leader>/', ':Telescope live_grep<CR>')
 
 -- Harpoon for fast and "consistent" file access.
 vim.keymap.set('n', '<leader>aa', function() require("harpoon.mark").add_file() end)
@@ -123,11 +67,11 @@ vim.keymap.set('n', '<M-r>', function() require("harpoon.ui").nav_file(3) end)
 vim.keymap.set('n', '<M-l>', function() require("harpoon.ui").nav_file(4) end)
 
 -- TODO: Everything from here on needs some organization and commenting.
-vim.keymap.set('n', '<leader>dt', function() vim.cmd("windo diffthis") end)
-vim.keymap.set('n', '<leader>do', function() vim.cmd("windo diffoff") end)
+vim.keymap.set('n', '<leader>dt', ':windo diffthis<CR>')
+vim.keymap.set('n', '<leader>do', ':windo diffoff<CR>')
 
-vim.keymap.set('n', '<leader>m', function () vim.cmd("Make") end)
-vim.keymap.set('n', '<leader>as', function () vim.cmd("TSHighlightCapturesUnderCursor") end)
+vim.keymap.set('n', '<leader>m',  ':Make')
+vim.keymap.set('n', '<leader>as', ':TSHighlightCapturesUnderCursor')
 
 
 vim.keymap.set('n', '<leader>ck', function ()
